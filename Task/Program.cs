b1edbd2b3d5b62ced058ba017f9b12ae694f474c
Kirill <frantic.87@mail.ru>
2023-07-03 17:52:31 +0300

    added ReadArrStr method

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ReadArrStr(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine().Split(", ");
+}
